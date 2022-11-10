73a5959a7c6f1cc8e112d2a1d159e51131daf9b7
Matheus Hernandes <12698656+onhernandes@users.noreply.github.com>
2022-11-10 00:10:18 -0300

    fixup! init

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
+
 import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useSignal("");
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
-          Edit <code>src/App.jsx</code> and save to reload.
+          A lista de produtos com aquele precinho que você esperava. Criado por @onhernandes.
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+
+        <input className="input" type="text" placeholder="Busque um termo" />
       </header>
+
+      <section className="results"></section>
     </div>
   );
 }
